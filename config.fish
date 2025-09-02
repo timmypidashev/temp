e5d3758c2b67dd1f35afba61934d9dd174e04620
Timothy Pidashev <mail@timmypidashev.dev>
2025-09-02 14:11:05 -0700

    update config

--- config.fish
+++ config.fish
@@ -2,6 +2,12 @@ set -g fish_greeting
 set -x GPG_TTY (tty)
 alias ls="ls --group-directories-first --color=auto"
 
+if not test -d ~/.config/nvim; and test -d /etc/xdg/nvim
+    mkdir -p ~/.config
+    cp -r /etc/xdg/nvim ~/.config/nvim
+    chmod -R u+w ~/.config/nvim
+end
+
 if test (tty) = "/dev/tty1"
   startx
 end
